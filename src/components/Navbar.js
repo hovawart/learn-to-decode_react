873abe9488199068323d92704842b82dd8eba75a
hovawart <ajc100514@gmail.com>
2023-09-18 16:07:54 -0600

    Updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className={classes.navbar}>
       <h2 className={classes.navLogo}>Learn to Decode</h2>
-      <div className={classes.navLinks}>
+      <div className={classes.navLink}>
         <a className={classes.navLink}>Home</a>
         <a className={classes.navLink}>Subjects</a>
         <a className={classes.navLink}>About</a>
